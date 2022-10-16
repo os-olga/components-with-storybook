c377f32f0fb586693240d0a83c75049df977b173
oliandrik <os.shcherbakovaa@gmail.com>
2022-10-16 13:29:26 +0300

    story typography

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -30,50 +30,9 @@ export default {
 
 const Template: Story<TypographyProps> = (args) => <Typography {...args} />;
 
-export const h1 = Template.bind({});
-h1.args = {
-  children: "Heading 1",
+export const typography = Template.bind({});
+typography.args = {
+  children:
+    "Spicy jalapeno bacon ipsum dolor amet swine tenderloin andouille, beef ribs rump prosciutto alcatra hamburger jerky.",
   variant: TypographyTypeEnum.h1,
-};
-
-export const h2 = Template.bind({});
-h2.args = {
-  children: "Heading 2",
-  variant: TypographyTypeEnum.h2,
-};
-
-export const h3 = Template.bind({});
-h3.args = {
-  children: "Heading 3",
-  variant: TypographyTypeEnum.h3,
-};
-
-export const h4 = Template.bind({});
-h4.args = {
-  children: "Heading 4",
-  variant: TypographyTypeEnum.h4,
-};
-
-export const h5 = Template.bind({});
-h5.args = {
-  children: "Heading 5",
-  variant: TypographyTypeEnum.h5,
-};
-
-export const sm = Template.bind({});
-sm.args = {
-  children: "Text sub_m",
-  variant: TypographyTypeEnum.sub_m,
-};
-
-export const ss = Template.bind({});
-ss.args = {
-  children: "text sm",
-  variant: TypographyTypeEnum.sub_s,
-};
-
-export const caption = Template.bind({});
-caption.args = {
-  children: "Caption",
-  variant: TypographyTypeEnum.caption,
 };
